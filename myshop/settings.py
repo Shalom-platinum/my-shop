11fe52950b09d220c9a4b52da0aed3dd352c69b6
Shalom-platinum <72386382+Shalom-platinum@users.noreply.github.com>
2022-07-23 13:43:24 +0100

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'dt!=v8m=1=g@(vc_822cotz^ipp9$^d^#!%)f*e##^g-zwwy1_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','shalomayshop.herokuapp.com']
 
